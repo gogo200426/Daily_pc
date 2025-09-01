357b0fe031fdf5d65a5653a69cd1c56784eab498
gogo200426 <fijashaikh6162@gmail.com>
2025-09-01 14:20:27 +0530

    Create Day 17.py
    Day 17 programming challenge

---
+++ Day 17.py
@@ -0,0 +1,13 @@
+def factors(n):
+    result = []
+    i = 2
+    while i * i <= n:
+        while n % i == 0:
+            result.append(i)
+            n //= i
+        i += 1
+    if n > 1:
+        result.append(n)
+    return result
+n = 18
+print("Prime factors of", n, "are:", factors(n))
